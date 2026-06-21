767734e692c0d22d73b60dd4a66ef736712ac28a
Frogytvv <elias@superandreas.com>
2026-06-21 21:35:49 +0200

    CHanges

--- src/ui/components/searchbar.tsx
+++ src/ui/components/searchbar.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import SearchIcon from "../assets/search.svg";
+import { useRef } from "react";
 
 function Searchbar() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        minWidth: "16rem",
-        maxWidth: "25rem",
-        gap: "0.6rem",
-      }}
-      className="searchbar"
-    >
-      <img src={SearchIcon} style={{ width: "18px" }} />
+    <div onClick={() => inputRef.current?.focus()} className="searchbar">
+      <img src={SearchIcon} style={{ width: "22px" }} />
       <input
-        style={{ flexGrow: 1, border: "none", backgroundColor: "inherit" }}
+        ref={inputRef}
+        style={{
+          flexGrow: 1,
+          border: "none",
+          backgroundColor: "transparent",
+          outline: "none",
+          color: "white",
+        }}
         type="search"
         placeholder="Search..."
       />
