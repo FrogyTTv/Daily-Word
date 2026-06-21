5c80fdaf7d719f4c3ce044407e57d5dadd597c7b
Frogytvv <elias@superandreas.com>
2026-06-21 20:44:27 +0200

    Added searchbar styling

--- src/ui/components/searchbar.tsx
+++ src/ui/components/searchbar.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import SearchIcon from "../assets/search.svg";
 
 function Searchbar() {
   return (
-    <>
-      <p>Search...</p>
-    </>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        minWidth: "16rem",
+        maxWidth: "25rem",
+        gap: "0.6rem",
+      }}
+      className="searchbar"
+    >
+      <img src={SearchIcon} style={{ width: "18px" }} />
+      <input
+        style={{ flexGrow: 1, border: "none", backgroundColor: "inherit" }}
+        type="search"
+        placeholder="Search..."
+      />
+      <p>⌘ + F</p>
+    </div>
   );
 }
 
