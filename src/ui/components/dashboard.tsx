6b1d39d15672e2fb02230bb632056ae564c39a64
Frogytvv <elias@superandreas.com>
2026-06-22 21:26:27 +0200

    Retrieved username from database.

--- src/ui/components/dashboard.tsx
+++ src/ui/components/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import type { Database } from "../global";
 
 const Dashboard = () => {
   const [dailyVerse, setDailyVerse] = useState({ text: "", reference: "" });
@@ -7,10 +8,12 @@ const Dashboard = () => {
 
   const BGColors = ["#D8E0E8", "#DEEAD6", "#FDF5C8", "#E8DDF5"];
   const [randomDVColor, setRandomDVColor] = useState("");
+  const [database, setDatabase] = useState<Database | null>(null);
 
   useEffect(() => {
     fetchDailyVerse();
     setRandomDVColor(BGColors[Math.floor(Math.random() * 4)]);
+    window.api.loadDatabase().then(setDatabase);
   }, []);
 
   async function fetchDailyVerse() {
@@ -33,7 +36,7 @@ const Dashboard = () => {
   }
   return (
     <>
-      <h1 style={{ padding: 0 }}>Hello, Elias 👋</h1>
+      <h1 style={{ padding: 0 }}>Hello, {database?.username} 👋</h1>
       <h2>Quick Stats</h2>
       <h2>Daily Verse</h2>
       <div
