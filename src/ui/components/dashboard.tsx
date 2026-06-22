be6e7c10b7d717b942b06db74e6385a7c73115f5
Frogytvv <elias@superandreas.com>
2026-06-22 19:29:08 +0200

    Added styling with color for the daily verse function

--- src/ui/components/dashboard.tsx
+++ src/ui/components/dashboard.tsx
@@ -5,6 +5,8 @@ const Dashboard = () => {
   const DAILYVERSEAPI =
     "https://beta.ourmanna.com/api/v1/get/?format=json&order=daily";
 
+  const DVColors = ["#D8E0E8", "#DEEAD6", "#FDF5C8", "#E8DDF5"];
+
   useEffect(() => {
     // Fetch Pokemons
     fetchDailyVerse();
@@ -32,8 +34,19 @@ const Dashboard = () => {
       <h1 style={{ padding: 0 }}>Hello, Elias 👋</h1>
       <h2>Quick Stats</h2>
       <h2>Daily Verse</h2>
-      <p>{dailyVerse.text}</p>
-      <p>{dailyVerse.reference}</p>
+      <div
+        className="dv-text"
+        style={{
+          backgroundColor: `${DVColors[Math.floor(Math.random() * 4)]}`,
+        }}
+      >
+        <h4>
+          {dailyVerse.text
+            ? `"${dailyVerse.text}"`
+            : "Daily Verse is Loading..."}
+        </h4>
+        <p>{dailyVerse.reference}</p>
+      </div>
     </>
   );
 };
